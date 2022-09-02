a48dc08b1984c172437d73506c1cb2873f279ca4
AlayaElla <515451867@qq.com>
2022-09-02 15:41:15 +0800

    Add files via upload

--- UI/GridHelperUI.cs
+++ UI/GridHelperUI.cs
@@ -6,16 +6,21 @@ public class GridHelperUI : LayeroutHelperUIBase
 {
     public enum ChildAligment
     {
-        Upper_Left
+        Upper_Left,
+        Upper_Center
     }
     public ChildAligment childAligment = ChildAligment.Upper_Left;
     public Vector2 spacing = new Vector2();
     public override void SetGrid()
     {
+        float current_width = 0;
+        int currentCount = 0;
+        float row_width = CalRowWidth(currentCount);
+        Vector3 _pos = Vector3.zero;
+
         float offsetWidth = 0 + padding.Left;
         float offsetHeight = 0 - padding.Top;
-        float current_width = 0;
-        Vector3 _pos = Vector3.zero;
+
         foreach (var item in childList)
         {
             if (item == null)
@@ -23,21 +28,25 @@ public class GridHelperUI : LayeroutHelperUIBase
 
             //如果超过容器长度则换行
             current_width = offsetWidth + item.rectTransform.rect.width + padding.Right;
-            if (current_width > rootRect.rect.width)
+            if (currentCount == 0 || current_width > rootRect.rect.width)
             {
-                offsetWidth = 0 + padding.Left;
-                offsetHeight -= spacing.y;
+                offsetHeight -= currentCount == 0 ? 0 : spacing.y;
+                if (childAligment == ChildAligment.Upper_Left)
+                {
+                    offsetWidth = 0 + padding.Left;
+                }
+                else if (childAligment == ChildAligment.Upper_Center)
+                {
+                    row_width = CalRowWidth(currentCount);
+                    offsetWidth = 0 + padding.Left + rootRect.rect.width / 2 - Mathf.Max(row_width, item.rectTransform.rect.width) / 2;
+                }
             }
-            if (childAligment == ChildAligment.Upper_Left)
-            {
-                _pos = new Vector3(
-                    offsetWidth + item.rectTransform.rect.width / 2,//x
-                    offsetHeight,//y
-                    0
-                    );
-
-                offsetWidth += item.rectTransform.rect.width + spacing.x;
-            }
+            _pos = new Vector3(
+                offsetWidth + item.rectTransform.rect.width / 2,//x
+                offsetHeight,//y
+                0);
+            offsetWidth += item.rectTransform.rect.width + spacing.x;
+            currentCount++;
 
 #if UNITY_EDITOR
             if (!Application.isPlaying)
@@ -59,5 +68,16 @@ public class GridHelperUI : LayeroutHelperUIBase
         SetGrid();
     }
 
+    float CalRowWidth(int startIndex)
+    {
+        float width = 0;
+        for (int i = startIndex; i < childList.Count; i++)
+        {
+            if (width + spacing.x + childList[i].rectTransform.rect.width >= rootRect.rect.width)
+                return width - spacing.x;
+            width += spacing.x + childList[i].rectTransform.rect.width;
+        }
+        return width - spacing.x;
+    }
 
 }
